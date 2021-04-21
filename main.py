ff9bcdc4f99611f0bc444e6ab6510facdef5ecb8
darius.lewen <darius.lewen@stud.uni-goettingen.de>
2021-04-21 13:04:30 +0200

    omptimized update() runtime with numba/jit

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import random as r
 import numpy as np
 from threading import Lock
+import time
+import copy
+
+from numba import njit
 
 import pyglet
 from pyglet import clock
@@ -30,12 +34,11 @@ attractors = [np.array([0,0],
 
 lasso = Lasso(targets)
 
-
+@njit
 def gaussian(x,mu=0.,sig=float(win.get_size()[0]/2)):
     return 1./(np.sqrt(2.*np.pi)*sig)*np.exp(-np.power((x-mu)/sig,2.)/2)
 
 def add_ants(dt):
-
     start_vel=8.
     width, height = win.get_size()
     
@@ -63,30 +66,71 @@ def add_ants(dt):
     upper_ant=Ant(win.get_size(),upper_pos,upper_vel)
     targets.append(upper_ant)
 
-def update(dt):
-    
+update_time = 0.
+draw_time = 0.
+
+@njit
+def absorb_calc(ants):
     absorber_idx = 0
-
-    while(absorber_idx<len(targets)): #TODO This goes better!
-        absorber=targets[absorber_idx]
-        idx_to_del = []
-        for i,ant in enumerate(targets[absorber_idx+1:]):
-            dist=np.linalg.norm(ant.pos-absorber.pos)
-            if dist < 20:
-                absorber.absorb(ant)
-                idx_to_del.append(i+absorber_idx+1)
-        for idx in sorted(idx_to_del, reverse=True):
-            del targets[idx]
+    #out = [] 
+    absorbers=[]#TODO fix length for jit?
+    absorbed=[]
+    
+    while(absorber_idx<len(ants)):
+        absorber=ants[absorber_idx]
+        if absorber[0]!=-42:
+            for i,ant in enumerate(ants[absorber_idx+1:]):
+                if ant[0]!=-42:
+                    dist=np.linalg.norm(ant-absorber)
+                    if dist < 20:
+                        #out.append((absorber_idx,i+1+absorber_idx))
+                        absorbers.append(absorber_idx)
+                        absorbed.append(i+1+absorber_idx)
+                        ants[i+1+absorber_idx] = (-42,-42)
         absorber_idx+=1
-            
+    return absorbers, absorbed
 
-    for ant in targets:
+@njit
+def accelerations_calc(ants, attractors):
+    accs=np.zeros_like(ants)
+    for i,ant in enumerate(ants):
         acc = np.array([0,0],dtype='float64')
         for atr in attractors:
-            vec=atr-ant.pos
+            vec=atr-ant
             dist=np.linalg.norm(vec)
             acc+=vec*gaussian(dist)/dist/gaussian(0.)
-        ant.update(acc)
+        accs[i]=acc
+    return accs
+
+
+#to run njit compiler
+absorb_calc(np.array([(42.,42.),(1.,1.)]))
+gaussian(0.)
+accelerations_calc(np.array([(42.,42.),(1.,1.)]), attractors)
+
+def ant_posistions():
+    out=targets[0].pos
+    for i in range(1,len(targets)):
+        out=np.vstack((out,targets[i].pos))
+    return out
+
+def update(dt):
+    if len(targets)==0:
+        return
+    
+    eater,food = absorb_calc(ant_posistions())
+
+    for e,f in zip(eater,food):
+        targets[e].absorb(targets[f])
+    for f in sorted(food, reverse=True):
+        del targets[f]
+    #for _,food in reversed(absorbings):
+    #    del targets[food] #TODO list assignment out of range
+
+    accelerations = accelerations_calc(ant_posistions(), attractors)
+    
+    for i,ant in enumerate(targets):
+        ant.update(accelerations[i])
 
 def switch_attractors(dt):
     global current_atr
@@ -101,15 +145,17 @@ def switch_attractors(dt):
             dtype='float64')
 
 
-clock.schedule_interval(add_ants, .15)
+clock.schedule_interval(add_ants, .2)
 clock.schedule_interval(update, 1/33)
 clock.schedule_interval(switch_attractors, 1)
 
 
 @win.event
 def on_draw():
+    begin = time.time()
+    global draw_time
+
     win.clear()
-    
     background = Rectangle(0,0,*win.get_size(),color=(255,255,255))
     background.draw()
     mouse_circle.draw()
@@ -117,16 +163,31 @@ def on_draw():
         ant.draw()
     lasso.draw()
 
+    t=time.time()-begin
+    if t>draw_time:
+        draw_time=t
+        print(f'draw_time:{draw_time}')
+
+
+drag_dist=0
 
 @win.event
 def on_mouse_drag(x, y, dx, dy, buttons, modifiers):
-    mouse_circle.anchor_position=(x,y)
-    lasso.consume((x,y))
+    global drag_dist
+    drag_dist+=(dx**2+dy**2)**(.5)
+    if drag_dist>10: #Fixes lag for high res mouse input
+        mouse_circle.anchor_position=(x,y)
+        lasso.consume((x,y))
+        drag_dist=0
 
 
 @win.event
 def on_mouse_release(x, y, button, modifiers):
     lasso.collapse()
+    #p = []
+    #for a in targets:
+    #    p.append(tuple(a.pos.astype('float64')))
+    #print(p)
 
 
 pyglet.app.run()
