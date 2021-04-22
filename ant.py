0e16316017990a9a77eb279307d03b16bb96f6c0
darius.lewen <darius.lewen@stud.uni-goettingen.de>
2021-04-22 12:35:34 +0200

    replaced lasso catching with point'n'click, imporoved point growth and point collision calculation

--- ant.py
+++ ant.py
@@ -3,37 +3,39 @@ import numpy as np
 from pyglet.shapes import Circle
 
 class Ant:
-    def __init__(self,win_size,pos=None,vel=None,size=1,base_speed=8):
+    def __init__(self,win_size,pos=None,vel=None,batch=None,size=1,base_speed=8):
         self.pos=pos
         self.vel=vel
         self.size=size
+        self.radius=(81*self.size)**(.5)
         self.win_size=win_size
-        #if not pos:
-        #    self.pos=np.array([win_size[0]//2,0],dtype='float64')
-        #    angle=np.random.rand()*np.pi/4+np.pi*3/8
-        #    self.vel=np.array([np.cos(angle),np.sin(angle)])*base_speed
 
+        self.circ=Circle(*self.pos,(81*self.size)**(.5),color=(100,100,200),batch=batch)
 
     def update(self, acceleration=np.array([0,0])):
-        self.vel*=.95 #air drag
+        self.vel*=(.96-self.size/200) #air drag
         self.vel+=acceleration
         self.pos+=self.vel
         if not 0<=self.pos[0]<=self.win_size[0]:
             self.vel[0]*=-1
         if not 0<=self.pos[1]<=self.win_size[1]:
             self.vel[1]*=-1
+        self.circ.position=tuple(self.pos)
 
 
     def absorb(self,ant):
-        self.size+=ant.size
-        self.pos+=ant.pos
-        self.vel+=ant.vel
-        self.pos/=2
-        self.vel/=2
+        s=self.size+ant.size
+        self.pos=(self.size*self.pos+ant.size*ant.pos)/s
+        self.vel=(self.size*self.vel+ant.size*ant.vel)/s
+        self.size=s
+        self.radius=(81*self.size)**(.5)
+        self.circ.position=tuple(self.pos)
+        self.circ.radius=self.radius
+        ant.circ.delete() #delete absorbed ant
 
 
-    def draw(self):
-        Circle(*self.pos,8+self.size,color=(100,100,200)).draw()
+#    def draw(self):
+#        Circle(*self.pos,8+self.size,color=(100,100,200)).draw()
 
     
     def coords(self):
--- main.py
+++ main.py
@@ -10,16 +10,27 @@ import pyglet
 from pyglet import clock
 from pyglet.shapes import Circle
 from pyglet.shapes import Rectangle
+from pyglet.graphics import Batch
+from pyglet.text import Label
 
-import shapely
-from shapely.geometry import LineString
-from shapely.geometry import Polygon
-from shapely.geometry import Point
-
-from lasso import Lasso
 from ant import Ant
 
 win = pyglet.window.Window()
+
+label_batch = Batch()
+ant_batch = Batch()
+catched_label = Label(text='Ants catched: ',
+        font_name='Arial',
+        font_size=20,
+        color=(0, 0, 0, 255),
+        x=0, y=0,
+        batch=label_batch)
+fps_label = Label(text='FPS: ',
+        font_name='Arial',
+        font_size=20,
+        color=(0, 0, 0, 255),
+        x=0, y=win.get_size()[1]-20,
+        batch=label_batch)
 
 #Event Logger
 #event_logger = pyglet.window.event.WindowEventLogger()
@@ -27,50 +38,39 @@ win = pyglet.window.Window()
 
 mouse_circle = Circle(0,0,5,color=(100,100,100))
 
+catch_count = 0
 targets = []
 current_atr = 0
 attractors = [np.array([0,0],
     dtype='float64')]*2
-
-lasso = Lasso(targets)
+angle_pendulum=0
 
 @njit
 def gaussian(x,mu=0.,sig=float(win.get_size()[0]/2)):
     return 1./(np.sqrt(2.*np.pi)*sig)*np.exp(-np.power((x-mu)/sig,2.)/2)
 
+def add_ant(x,y,angle):
+    pos=np.array([x,y],dtype='float64')
+    vel=np.array([np.cos(angle),np.sin(angle)])*10
+    ant=Ant(win.get_size(),pos,vel,batch=ant_batch)
+    targets.append(ant)
+
+
 def add_ants(dt):
-    start_vel=8.
+    global angle_pendulum
     width, height = win.get_size()
-    
-    angle=np.random.rand()*np.pi/4+np.pi*5/8
 
-    left_pos=np.array([0,height/2],dtype='float64')
-    left_vel=np.array([-np.cos(angle),np.sin(angle)])*start_vel
-    left_ant=Ant(win.get_size(),left_pos,left_vel)
-    targets.append(left_ant)
+    angle=np.pi/2*np.sin(np.pi*angle_pendulum/20)-np.pi/4
+    add_ant(0,height/2,angle)
+    add_ant(width/2,0,angle+np.pi/2)
+    add_ant(width,height/2,angle+np.pi)
+    add_ant(width/2,height,angle+np.pi*3/2)
 
-    right_pos=np.array([width,height/2],dtype='float64')
-    right_vel=np.array([np.cos(angle),np.sin(angle)])*start_vel
-    right_ant=Ant(win.get_size(),right_pos,right_vel)
-    targets.append(right_ant)
-    
-    angle=np.random.rand()*np.pi/4+np.pi*3/8
-
-    lower_pos=np.array([width/2,0],dtype='float64')
-    lower_vel=np.array([-np.cos(angle),np.sin(angle)])*start_vel
-    lower_ant=Ant(win.get_size(),lower_pos,lower_vel)
-    targets.append(lower_ant)
-    
-    upper_pos=np.array([width/2,height],dtype='float64')
-    upper_vel=np.array([-np.cos(angle),-np.sin(angle)])*start_vel
-    upper_ant=Ant(win.get_size(),upper_pos,upper_vel)
-    targets.append(upper_ant)
-
-update_time = 0.
-draw_time = 0.
+    angle_pendulum+=1
+    angle_pendulum%=20
 
 @njit
-def absorb_calc(ants):
+def absorb_calc(ants,radians):
     absorber_idx = 0
     #out = [] 
     absorbers=[]#TODO fix length for jit?
@@ -82,7 +82,8 @@ def absorb_calc(ants):
             for i,ant in enumerate(ants[absorber_idx+1:]):
                 if ant[0]!=-42:
                     dist=np.linalg.norm(ant-absorber)
-                    if dist < 20:
+                    #if dist < 20:
+                    if dist < radians[absorber_idx]+radians[i+1+absorber_idx]:
                         #out.append((absorber_idx,i+1+absorber_idx))
                         absorbers.append(absorber_idx)
                         absorbed.append(i+1+absorber_idx)
@@ -102,23 +103,37 @@ def accelerations_calc(ants, attractors):
         accs[i]=acc
     return accs
 
+@njit
+def shot(pos,ants,radians):
+    for i,ant in enumerate(ants):
+        dist=np.linalg.norm(ant-pos)
+        if dist < radians[i]:
+            return i
+
 
 #to run njit compiler
-absorb_calc(np.array([(42.,42.),(1.,1.)]))
+absorb_calc(np.array([(42.,42.),(1.,1.)]),np.array([[42.],[42.]]))
 gaussian(0.)
 accelerations_calc(np.array([(42.,42.),(1.,1.)]), attractors)
+shot(np.array((0,0)),np.array([(42.,42.),(1.,1.)]),np.array(([42.],[42.])))
 
 def ant_posistions():
-    out=targets[0].pos
+    out=np.array([targets[0].pos])
     for i in range(1,len(targets)):
-        out=np.vstack((out,targets[i].pos))
+        out=np.vstack((out,np.array([targets[i].pos])))
+    return out
+
+def ant_radians():
+    out=np.array([targets[0].radius])
+    for i in range(1,len(targets)):
+        out=np.vstack((out,np.array([targets[i].radius])))
     return out
 
 def update(dt):
     if len(targets)==0:
         return
-    
-    eater,food = absorb_calc(ant_posistions())
+   
+    eater,food = absorb_calc(ant_posistions(),ant_radians())
 
     for e,f in zip(eater,food):
         targets[e].absorb(targets[f])
@@ -146,48 +161,34 @@ def switch_attractors(dt):
 
 
 clock.schedule_interval(add_ants, .2)
-clock.schedule_interval(update, 1/33)
+clock.schedule_interval(update, 1/60)
 clock.schedule_interval(switch_attractors, 1)
 
 
 @win.event
 def on_draw():
-    begin = time.time()
-    global draw_time
-
     win.clear()
     background = Rectangle(0,0,*win.get_size(),color=(255,255,255))
     background.draw()
+
+    ant_batch.draw()
     mouse_circle.draw()
-    for ant in targets:
-        ant.draw()
-    lasso.draw()
+    global catch_count 
 
-    t=time.time()-begin
-    if t>draw_time:
-        draw_time=t
-        print(f'draw_time:{draw_time}')
-
-
-drag_dist=0
-
-@win.event
-def on_mouse_drag(x, y, dx, dy, buttons, modifiers):
-    global drag_dist
-    drag_dist+=(dx**2+dy**2)**(.5)
-    if drag_dist>10: #Fixes lag for high res mouse input
-        mouse_circle.anchor_position=(x,y)
-        lasso.consume((x,y))
-        drag_dist=0
+    catched_label.text=f'Ants catched: {catch_count}'
+    fps_label.text=f'FPS: {clock.get_fps()}'
+    fps_label.y=win.get_size()[1]-20
+    label_batch.draw()
 
 
 @win.event
-def on_mouse_release(x, y, button, modifiers):
-    lasso.collapse()
-    #p = []
-    #for a in targets:
-    #    p.append(tuple(a.pos.astype('float64')))
-    #print(p)
+def on_mouse_press(x, y, scroll_x, scroll_y):
+    mouse_circle.position=(x,y)
+    i = shot(np.array((x,y)),ant_posistions(),ant_radians())
+    global catch_count
+    if i is not None:
+        catch_count+=targets[i].size
+        del targets[i]
 
 
 pyglet.app.run()
