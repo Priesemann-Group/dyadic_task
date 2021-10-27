ee191421b48232098f6bc0dcd6a26ecc7ef7f562
darius.lewen <darius.lewen@stud.uni-goettingen.de>
2021-10-27 11:41:05 +0200

    Fixed inverted mouse control.

--- src/replay.py
+++ src/replay.py
@@ -78,10 +78,10 @@ class Replay:
     def _next_frame(self):
         packet = self._record[self._current_frame]
         packet = packet[1:]  # throw first, general header away
-        prediction = self._predictor.predict(
-            ant_pos=packet[3:, :2],
-            p0_pos=packet[0, :2],
-            p1_pos=packet[1, :2])
+        #prediction = self._predictor.predict(
+        #    ant_pos=packet[3:, :2],
+        #    p0_pos=packet[0, :2],
+        #    p1_pos=packet[1, :2])
         self._ui.set_values(
             player_0_pos=packet[0, :2],
             player_1_pos=packet[1, :2],
@@ -90,8 +90,8 @@ class Replay:
             scores=list(packet[2, :2]),
             pings=list(packet[:2, 3]),
             ant_pos=packet[3:, :2],
-            ant_kinds=packet[3:, 2],
-            prediction=prediction)
+            ant_kinds=packet[3:, 2])
+            #prediction=prediction)
         self._current_frame += 1
 
 
